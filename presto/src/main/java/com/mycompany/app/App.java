648ca0e86f0deea80713d12d008ef16d8b2b1eb0
v-jizhang <jzhang@openteams.com>
2022-09-27 13:38:06 -0700

    Push permission test

--- presto/src/main/java/com/mycompany/app/App.java
+++ presto/src/main/java/com/mycompany/app/App.java
@@ -17,6 +17,6 @@ public class App
     public static void main( String[] args )
     {
         Arrays.stream(args).forEach(System.out::println);
-        System.out.println( "Hello World!" );
+        System.out.println("Hello World!");
     }
 }
